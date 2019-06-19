2d427d52569eb1fa7a785248e073a1cf73378986
Aaqib Shehzad <aaqibshehzad>
2019-06-19 12:04:13 -0300

    Changed static folder routing.

--- api/server.py
+++ api/server.py
@@ -1,17 +1,22 @@
 # server.py
 from flask import Flask, render_template, jsonify
 
-app = Flask(__name__, static_folder="../static/dist", template_folder="../static/dist")
+app = Flask(
+    __name__, static_url_path="", static_folder="./static", template_folder="./static"
+)
+
 
 @app.route("/")
 def index():
-    return render_template("index.html", data='Hello')
+    return render_template("index.html", data="Hello")
 
-@app.route("/hello", methods=['POST'])
+
+@app.route("/hello", methods=["POST"])
 def hello():
-    data = 'Flask React'
+    data = "Flask React"
     return jsonify(data)
 
+
 if __name__ == "__main__":
-    app.debug=True
+    app.debug = True
     app.run()
